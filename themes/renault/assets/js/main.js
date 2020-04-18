cf8fe3a8e2bffdd048a69444124b153b0485578f
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-04-19 01:26:49 +0600

    contact page desktop view done

--- themes/renault/assets/js/main.js
+++ themes/renault/assets/js/main.js
@@ -146,7 +146,62 @@ google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 /* Start Milon   --->*/
+/*
+----------------------
+ Tabs Js
+----------------------
+*/
+if( $('.tabs').length ){
+  $('.tabs:first').show();
+  $('.info-tabs-menu li:first').addClass('active');
 
+  $('.info-tabs-menu li').on('click',function(){
+    index = $(this).index();
+    $('.info-tabs-menu li').removeClass('active');
+    $(this).addClass('active');
+    $('.tabs').hide();
+    $('.tabs').eq(index).show();
+  });
+}
+
+
+/*
+-----------------------
+Start Contact Google Map ->> 
+-----------------------
+*/
+if( $('#googlemap').length ){
+    var latitude = $('#googlemap').data('latitude');
+    var longitude = $('#googlemap').data('longitude');
+
+    var myCenter= new google.maps.LatLng(latitude,  longitude);
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    function initialize(){
+        var mapProp = {
+          center:myCenter,
+
+          mapTypeControl:false,
+          scrollwheel: false,
+
+          zoomControl: false,
+          disableDefaultUI: true,
+          zoom:17,
+          streetViewControl: false,
+          rotateControl: false,
+          mapTypeId:google.maps.MapTypeId.ROADMAP,
+          styles : CustomMapStyles
+      };
+      var map= new google.maps.Map(document.getElementById('googlemap'),mapProp);
+
+      var marker= new google.maps.Marker({
+        position:myCenter,
+        icon:'assets/images/map-marker.png'
+        });
+      marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+}
 
 
 
