33311ef81e7646903d14b8f8d771cbbd78d4f747
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2020-04-20 00:35:55 +0600

    tweedehands details page desktop view done

--- themes/renault/assets/js/main.js
+++ themes/renault/assets/js/main.js
@@ -156,7 +156,32 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 /*Start Shoriful  ---> */
 
-
+if( $('.rn-td-slider-inr').length ){
+    $('.rn-td-slider-inr').slick({
+      pauseOnHover: false,
+      dots: false,
+      infinite: true,
+      arrows: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.rn-td-slider-bottom',
+      prevArrow: $('.rn-td-mainslider .left-arrow'),
+      nextArrow: $('.rn-td-mainslider .right-arrow'),
+    });
+    $('.rn-td-slider-bottom').slick({
+      pauseOnHover: false,
+      dots: false,
+      infinite: true,
+      arrows: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.rn-td-slider-inr',
+      focusOnSelect: true,
+      centerMode: true,
+    });
+}
 
 
 
