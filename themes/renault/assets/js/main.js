d68cc8cea21f4718ea7d08b21c9441bdf5379505
Rannojit <cserannojitsarkar22@gmail.com>
2020-04-22 21:18:57 +0600

    ..

--- themes/renault/assets/js/main.js
+++ themes/renault/assets/js/main.js
@@ -446,6 +446,44 @@ if( $('.hmVerkoopSlider').length ){
     });
 }
 
+if( $('.dftProItemsSlider').length ){
+    $('.dftProItemsSlider').slick({
+      dots: false,
+      infinite: false,
+      arrows: false, 
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+
 if( $('.fl-tabs').length ){
   $('div.fl-tabs button').click(function(){
      $('.hmVerkoopSlider').slick('refresh');
